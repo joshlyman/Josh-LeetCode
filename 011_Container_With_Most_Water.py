961d659a874b243230b5ea0c9110da112b5b2875
joshlyman <yanzhexu@asu.edu>
2020-10-05 12:29:44 -0700

    011

---
+++ 011_Container_With_Most_Water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxarea = 0
+        start = 0
+        end = len(height)-1
+        
+        while start < end:
+            area = (end-start)*min(height[end],height[start])
+            
+            if area > maxarea:
+                maxarea = area
+            
+            if height[end]>height[start]:
+                start+=1
+            else:
+                end-=1
+         
+        return maxarea 
+
+# Time: O(n)
+# Space:O(n)
