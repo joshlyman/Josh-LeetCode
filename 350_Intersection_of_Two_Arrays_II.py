d7894459a7de0b7189cca6bb7baa6b5662da41af
joshlyman <yanzhexu@asu.edu>
2020-10-08 04:08:28 -0700

    350

--- 350_Intersection_of_Two_Arrays_II.py
+++ 350_Intersection_of_Two_Arrays_II.py
@@ -1,13 +1,13 @@
-class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        interArray = []
+# class Solution:
+#     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         interArray = []
         
-        for i in nums1:
-            if i in nums2:
-                interArray.append(i)
-                nums2.remove(i)
+#         for i in nums1:
+#             if i in nums2:
+#                 interArray.append(i)
+#                 nums2.remove(i)
         
-        return interArray
+#         return interArray
 
 # Fast version:
 
@@ -15,3 +15,28 @@ class Solution:
 # class Solution:    
     # def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         # return (Counter(nums1) & Counter(nums2)).elements()
+
+# Fast version:
+
+class Solution:    
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # return (Counter(nums1) & Counter(nums2)).elements()
+        
+        if len(nums1)> len(nums2):
+            return self.intersect(nums2,nums1)
+        
+        lookup = collections.defaultdict(int)
+        
+        for i in nums1:
+            lookup[i]+=1
+            
+        results = []
+        for i in nums2:
+            if lookup[i]>0:
+                results.append(i)
+                lookup[i]-=1
+        
+        return results
+
+# Time: O(n)
+# Space: O(n)
