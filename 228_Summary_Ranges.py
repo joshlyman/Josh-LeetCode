ff9ca941d058d8ff8b07b52d25ddb69e0aaade81
joshlyman <yanzhexu@asu.edu>
2020-10-30 00:25:05 -0700

    228

---
+++ 228_Summary_Ranges.py
@@ -0,0 +1,24 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+        
+        res, i, start = [], 0, 0
+        
+        while i < len(nums)-1:
+            if nums[i]+1 != nums[i+1]:
+                res.append(self.printRange(nums[start], nums[i]))
+                start = i+1
+            i += 1
+        res.append(self.printRange(nums[start], nums[i]))
+        
+        return res
+
+    def printRange(self, l, r):
+        if l == r:
+            return str(l)
+        else:
+            return str(l) + "->" + str(r)
+
+# Time: O(N)
+# Space:O(1)
