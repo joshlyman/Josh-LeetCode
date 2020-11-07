61aeed10018e380002de9abe8d25daa8f1df54aa
joshlyman <yanzhexu@asu.edu>
2020-11-07 00:05:27 -0700

    151

---
+++ 151_Reverse_Words_in_a_String.py
@@ -0,0 +1,141 @@
+
+# Approach 1: use built-in function 
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return " ".join(reversed(s.split()))
+
+# V2
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return (" ".join(s.split()[::-1]))
+
+# Time: O(N)
+# Space:O(N)
+
+
+# Approach 2: Reverse the Whole String and Then Reverse Each Word
+class Solution:
+    def trim_spaces(self, s: str) -> list:
+        left, right = 0, len(s) - 1
+        # remove leading spaces
+        while left <= right and s[left] == ' ':
+            left += 1
+        
+        # remove trailing spaces
+        while left <= right and s[right] == ' ':
+            right -= 1
+        
+        # reduce multiple spaces to single one
+        output = []
+        while left <= right:
+            if s[left] != ' ':
+                output.append(s[left])
+            elif output[-1] != ' ':
+                output.append(s[left])
+            left += 1
+        
+        return output
+            
+    def reverse(self, l: list, left: int, right: int) -> None:
+        while left < right:
+            l[left], l[right] = l[right], l[left]
+            left, right = left + 1, right - 1
+            
+    def reverse_each_word(self, l: list) -> None:
+        n = len(l)
+        start = end = 0
+        
+        while start < n:
+            # go to the end of the word
+            while end < n and l[end] != ' ':
+                end += 1
+            # reverse the word
+            self.reverse(l, start, end - 1)
+            # move to the next word
+            start = end + 1
+            end += 1
+                
+    def reverseWords(self, s: str) -> str:
+        # converst string to char array 
+        # and trim spaces at the same time
+        l = self.trim_spaces(s)
+        
+        # reverse the whole string
+        self.reverse(l, 0, len(l) - 1)
+        
+        # reverse each word
+        self.reverse_each_word(l)
+        
+        return ''.join(l)
+
+
+
+# Time: O(N)
+# Space:O(N)
+
+
+
+# Approach 3: Reverse the Whole String and put each Word in string word and put word into words  
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        # return " ".join(reversed(s.split()))
+        
+        # First reverse entire string, then iterate over reversed string
+        # and again reverse order of characters within a word. Append each word to words.
+        word = ""
+        words = ""
+        s = s[::-1]
+        for j, i in enumerate(s):
+            # character is not space, a current word exists, 
+            # and previous character is space, e.g. i=b in " a b":
+            if i != " " and word != "" and s[j-1] == " ":
+                # add current word to words and append " " to later add this i
+                words += word
+                words += " "
+                word = i
+            # character is not space, but it's either first character in string
+            # or is part of current word, e.g. i=b in "b", " b" "ab", "a ab "
+            elif i != " ":
+                word = i + word
+            else:
+                continue
+
+        words += word
+        
+        return(words)
+
+# Time: O(N)
+# Space:O(N)
+
+
+# Approach 4: Deque of Words
+from collections import deque
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        left, right = 0, len(s) - 1
+        # remove leading spaces
+        while left <= right and s[left] == ' ':
+            left += 1
+        
+        # remove trailing spaces
+        while left <= right and s[right] == ' ':
+            right -= 1
+            
+        d, word = deque(), []
+        # push word by word in front of deque
+        while left <= right:
+            if s[left] == ' ' and word:
+                d.appendleft(''.join(word))
+                word = []
+            elif s[left] != ' ':
+                word.append(s[left])
+            left += 1
+        d.appendleft(''.join(word))
+        
+        return ' '.join(d)
+
+
+# Time: O(N)
+# Space:O(N)
+
+
