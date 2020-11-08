bf0797d49c419a481ee35c1ccffd852b41859156
joshlyman <yanzhexu@asu.edu>
2020-11-07 18:19:06 -0700

    695

---
+++ 695_Max_Area_of_Island.py
@@ -0,0 +1,48 @@
+
+# DFS using recursive  
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        def dfs(i, j):
+            if 0 <= i < m and 0 <= j < n and grid[i][j]:
+                grid[i][j] = 0
+                return 1 + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i + 1, j) + dfs(i, j - 1)
+            return 0
+
+        areas = [dfs(i, j) for i in range(m) for j in range(n) if grid[i][j]]
+        return max(areas) if areas else 0
+
+
+
+# Time: O(RC), where R is the number of rows in the given grid, and C is the number of columns. 
+# We visit every square once.
+
+# Space:O(RC)
+
+
+# DFS using iterative 
+
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        seen = set()
+        ans = 0
+        for r0, row in enumerate(grid):
+            for c0, val in enumerate(row):
+                if val and (r0, c0) not in seen:
+                    shape = 0
+                    stack = [(r0, c0)]
+                    seen.add((r0, c0))
+                    while stack:
+                        r, c = stack.pop()
+                        shape += 1
+                        for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+                            if (0 <= nr < len(grid) and 0 <= nc < len(grid[0])
+                                    and grid[nr][nc] and (nr, nc) not in seen):
+                                stack.append((nr, nc))
+                                seen.add((nr, nc))
+                    ans = max(ans, shape)
+        return ans
+
+# Time: O(RC)
+# Space:O(RC)
