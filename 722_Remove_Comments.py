e2fe8a748604de5ec19d3bbedcc8b92130dd3606
joshlyman <yanzhexu@asu.edu>
2020-11-13 19:30:49 -0700

    722

---
+++ 722_Remove_Comments.py
@@ -0,0 +1,31 @@
+# Parsing to skip all comments: in block or comments outside block 
+class Solution:
+    def removeComments(self, source: List[str]) -> List[str]:
+        in_block = False 
+        ans = []
+        
+        for line in source:
+            i = 0
+            if not in_block:
+                newline = []
+            
+            while i < len(line):
+                if line[i:i+2] == '/*' and not in_block:
+                    in_block = True 
+                    i+=1
+                elif line[i:i+2] == '*/' and in_block:
+                    in_block = False
+                    i+=1
+                elif not in_block and line[i:i+2] == '//':
+                    break
+                elif not in_block:
+                    newline.append(line[i])
+                
+                i+=1
+            if newline and not in_block:
+                ans.append("".join(newline))
+        
+        return ans 
+
+# Time: O(S), where S is the total length of the source code.
+# Space:O(S)
