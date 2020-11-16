d24340c69fe11b92250b07a5b2ca2b0d9ade5f28
joshlyman <yanzhexu@asu.edu>
2020-11-15 17:28:19 -0700

    079

---
+++ 079. Word Search.py
@@ -0,0 +1,43 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.ROWS = len(board)
+        self.COLS = len(board[0])
+        self.board = board
+
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.backtrack(row, col, word):
+                    return True
+
+        # no match found after all exploration
+        return False
+
+
+    def backtrack(self, row, col, suffix):
+        # bottom case: we find match for each letter in the word
+        if len(suffix) == 0:
+            return True
+
+        # Check the current status, before jumping into backtracking
+        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS or self.board[row][col] != suffix[0]:
+            return False
+
+        ret = False
+        # mark the choice before exploring further.
+        self.board[row][col] = '#'
+        # explore the 4 neighbor directions
+        for rowOffset, colOffset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            ret = self.backtrack(row + rowOffset, col + colOffset, suffix[1:])
+            # break instead of return directly to do some cleanup afterwards
+            # break means we find the word 
+            if ret: 
+                break
+
+        # if not break, means we have not find the target, so we need to cancel previous mark: # 
+        self.board[row][col] = suffix[0]
+
+        # Tried all directions, and did not find any match
+        return ret
+
+# Time: O(N*3^L) for worst case, N is # of cells in board, 3 directions for each letter in word, because we cannot go back
+# Space:O(L), L is length of matched word
