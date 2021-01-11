f7abe755e5bf494dcad6861d3741839f3a329855
joshlyman <yanzhexu@asu.edu>
2021-01-11 13:43:45 -0700

    update 21

--- 021_Merge_Two_Sorted_Lists.py
+++ 021_Merge_Two_Sorted_Lists.py
@@ -4,6 +4,8 @@
 #         self.val = x
 #         self.next = None
 
+
+# Iteration 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         dummy = ListNode(0)
@@ -26,4 +28,28 @@ class Solution:
         return dummy.next 
 
 # Time: O(n+m)
-# Space:O(n+m) 
+# Space:O(1)
+
+
+# Recursion 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+        elif l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
+        
+# Time: O(n+m)
+# Space:O(n+m)            
