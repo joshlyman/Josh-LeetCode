a9e05923b20963a17345ad4888c688adcfdad95c
joshlyman <yanzhexu@asu.edu>
2021-01-18 13:32:16 -0700

    253

--- 253_Meeting_Rooms_II.py
+++ 253_Meeting_Rooms_II.py
@@ -16,6 +16,57 @@ class Solution:
                 e +=1
         return roomnum 
 
-# Time: O(nlogn), sorting takes nlogn
-# Space:O(1)
+# Time: O(NlogN), sorting takes nlogn
+# Space:O(N)
+
+# V2: my version 
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        # start 0  5  15
+        # end   10 20 30
+        
+        start_time = sorted(x[0] for x in intervals)
+        end_time = sorted(x[1] for x in intervals)
+        
+        eidx = 0
+        roomnum = 0
+        
+        for sidx in range(len(start_time)):
+            if start_time[sidx] < end_time[eidx]:
+                roomnum +=1
+            else:
+                eidx +=1
+        
+        return roomnum 
+        
+# V2: use heap 
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        # start 0  5  15
+        # end   10 20 30
+        
+        # use heap to track the end of meeting time 
+        if not intervals:
+            return 0
+        
+        intervals.sort(key = lambda x:x[0])
+        
+        free_rooms = []
+        heapq.heapify(free_rooms)
+        # push the end time of first meeting into heap 
+        heapq.heappush(free_rooms,intervals[0][1])
+        
+        for i in intervals[1:]:
+            if free_rooms[0] <= i[0]:
+                heapq.heappop(free_rooms)
+        
+            heapq.heappush(free_rooms,i[1])
+        
+        return len(free_rooms)
+
+# Time: O(NlogN)
+# Space:O(N)
+
 
