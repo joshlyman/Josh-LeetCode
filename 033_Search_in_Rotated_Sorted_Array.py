242244df137ca6015f666dec9504eac574169835
joshlyman <yanzhexu@asu.edu>
2021-01-19 20:32:43 -0700

    35

--- 033_Search_in_Rotated_Sorted_Array.py
+++ 033_Search_in_Rotated_Sorted_Array.py
@@ -6,6 +6,9 @@ class Solution:
             mid = (begin + end)//2
             if nums[mid] == target:
                 return mid
+
+            # 4567012: mid:7,target:5,  
+            # 4560123: mid:0,target:5
             if nums[mid] > nums[end]: # Left side of mid is sorted
                 if  nums[begin] <= target and target < nums[mid]: # Target in the left side
                     end = mid - 1
